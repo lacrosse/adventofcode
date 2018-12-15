67e47f4b8a3afea0a24cb64f97480165f95399fb
Alexander Skiba <alex@aeriform.one>
2018-12-15 10:25:13 +0700

    Increase timeout for day 14 part 2

--- 14/test/chocolate_charts_test.exs
+++ 14/test/chocolate_charts_test.exs
@@ -1,8 +1,8 @@
 defmodule ChocolateChartsTest do
   use ExUnit.Case
 
-  @tag timeout: 10000
+  @tag timeout: 180_000
   test "solves" do
-    assert ChocolateCharts.solve() == {"8176111038", nil}
+    assert ChocolateCharts.solve() == {"8176111038", 20_225_578}
   end
 end
